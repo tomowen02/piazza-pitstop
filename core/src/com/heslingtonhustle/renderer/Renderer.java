af9112bb591af0768acf52108b5ea069b449cf75
Dan <dan@manby.org>
2024-03-14 13:12:45 +0000

    Refactored rendering code into its own class.

---
+++ core/src/com/heslingtonhustle/renderer/Renderer.java
@@ -0,0 +1,94 @@
+package com.heslingtonhustle.renderer;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.heslingtonhustle.map.MapManager;
+import com.heslingtonhustle.state.State;
+
+public class Renderer implements Disposable {
+
+    public static final int SCREEN_WIDTH = 640;
+    public static final int SCREEN_HEIGHT = 480;
+
+    private final ExtendViewport viewport;
+    private OrthographicCamera camera;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private final State gameState;
+    private SpriteBatch batch;
+    private Sprite playerSprite;
+    private TextureAtlas textureAtlas;
+
+    private final MapManager mapManager;
+
+    private final TextureRegion playerTexture;
+    private final TextureRegion buildingTexture;
+
+    public Renderer(State state, MapManager mapManager)
+    {
+        gameState = state;
+        camera = new OrthographicCamera();
+        this.mapManager = mapManager;
+        mapManager.loadMap("Maps/fieldMap.tmx"); // This is just a test map that I made
+        TiledMap map = mapManager.getCurrentMap();
+        batch = new SpriteBatch();
+        mapRenderer = new OrthogonalTiledMapRenderer(map, batch);
+        viewport = new ExtendViewport(30*16, 20*16, camera);
+        viewport.setScreenSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        //camera.viewportWidth = GAME_WIDTH;
+        //camera.viewportHeight = GAME_HEIGHT;
+
+
+        textureAtlas = new TextureAtlas("pack.atlas");
+        playerTexture = textureAtlas.findRegion("circle");
+        buildingTexture = textureAtlas.findRegion("triangle");
+
+        //playerTexture = new Texture("circle.png");
+        //playerSprite = new Sprite(playerTexture);
+        playerSprite = new Sprite(playerTexture);
+        playerSprite.setScale(0.5f, 0.5f);
+
+
+
+    }
+
+    public void update() {
+        /* float x = MathUtils.clamp(gameState.getPlayerPosition().x, camera.viewportWidth/2, GAME_WIDTH - camera.viewportWidth/2);
+        float y = MathUtils.clamp(gameState.getPlayerPosition().y, camera.viewportHeight/2, GAME_HEIGHT - camera.viewportHeight/2);
+        camera.position.set(x, y, 0); */
+        camera.position.set(gameState.getPlayerPosition(), 0);
+        viewport.update(SCREEN_WIDTH, SCREEN_HEIGHT);
+        batch.setProjectionMatrix(camera.combined);
+
+        ScreenUtils.clear(0.2f, 0.2f, 0.5f, 1);
+
+        mapRenderer.setView(camera);
+        mapRenderer.render();
+
+        //playerSprite.setRotation(gameState.getPlayerFacing());
+        playerSprite.setPosition(gameState.getPlayerPosition().x, gameState.getPlayerPosition().y);
+
+        batch.begin();
+        playerSprite.draw(batch);
+        batch.draw(buildingTexture, 50, 50);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        mapRenderer.dispose();
+        mapManager.dispose();
+    }
+
+    public void windowResized(int width, int height) {
+        viewport.update(width, height, true);
+    }
+}
--- core/src/com/heslingtonhustle/screens/PlayScreen.java
+++ core/src/com/heslingtonhustle/screens/PlayScreen.java
@@ -14,89 +14,44 @@ import com.heslingtonhustle.HeslingtonHustleGame;
 import com.heslingtonhustle.input.InputHandler;
 import com.heslingtonhustle.input.KeyboardInputHandler;
 import com.heslingtonhustle.map.MapManager;
+import com.heslingtonhustle.renderer.Renderer;
 import com.heslingtonhustle.state.Action;
 import com.heslingtonhustle.state.State;
 
 public class PlayScreen implements Screen {
     public HeslingtonHustleGame heslingtonHustleGame;
-    public static final int SCREEN_WIDTH = 640;
-    public static final int SCREEN_HEIGHT = 480;
-    public static final float GAME_WIDTH = 30*16;
-    public static final float GAME_HEIGHT = 20*16;
+
     private final InputHandler inputHandler;
     private final State gameState;
-    private final ExtendViewport viewport;
-    private OrthographicCamera camera;
-    private MapManager mapManager;
-    private OrthogonalTiledMapRenderer renderer;
-    SpriteBatch batch;
-    Texture playerTexture;
-    Sprite playerSprite;
+    private final Renderer renderer;
 
     public PlayScreen(HeslingtonHustleGame parentClass) {
         this.heslingtonHustleGame = parentClass;
 
-        camera = new OrthographicCamera();
-        viewport = new ExtendViewport(GAME_WIDTH, GAME_HEIGHT, camera);
-        viewport.setScreenSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-        camera.viewportWidth = GAME_WIDTH;
-        camera.viewportHeight = GAME_HEIGHT;
-
-        mapManager = new MapManager();
-        mapManager.loadMap("Maps/fieldMap.tmx"); // This is just a test map that I made
-
         gameState = new State();
         inputHandler = new KeyboardInputHandler();
+        renderer = new Renderer(gameState, new MapManager());
+
         Gdx.input.setInputProcessor(inputHandler);
-
-        TiledMap map = mapManager.getCurrentMap();
-        renderer = new OrthogonalTiledMapRenderer(map);
-
-        batch = new SpriteBatch();
-
-        playerTexture = new Texture("circle.png");
-        playerSprite = new Sprite(playerTexture);
-        playerSprite.setSize(1f*16, 1f*16);
     }
     @Override
     public void render(float delta) {
-        handleInput();
-        updateState();
-        drawToScreen();
+        gameState.update(inputHandler.getAction());
+        renderer.update();
     }
-
-    private void handleInput() {
-    }
-
-    private void updateState() {
-        Action action = inputHandler.getAction();
-        if (action == Action.DEBUGGING_ACTION1) {
-            mapManager.loadMap("Maps/caveMap.tmx");
-            renderer.setMap(mapManager.getCurrentMap());
-            return;
-        } else if (action == Action.DEBUGGING_ACTION2) {
-            mapManager.loadMap("Maps/fieldMap.tmx");
-            renderer.setMap(mapManager.getCurrentMap());
-            return;
-        }
-        gameState.update(action);
-    }
-
-    private void drawToScreen() {
-        camera.position.set(gameState.getPlayerPosition(), 0);
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
-
-        ScreenUtils.clear(0.2f, 0.2f, 0.5f, 1);
-
-        renderer.setView(camera);
-        renderer.render();
-
-        batch.begin();
-        gameState.getPlayerSprite().draw(batch);
-        batch.end();
-    }
-
+//    private void updateState() {
+//        Action action = inputHandler.getAction();
+//        if (action == Action.DEBUGGING_ACTION1) {
+//            mapManager.loadMap("Maps/caveMap.tmx");
+//            renderer.setMap(mapManager.getCurrentMap());
+//            return;
+//        } else if (action == Action.DEBUGGING_ACTION2) {
+//            mapManager.loadMap("Maps/fieldMap.tmx");
+//            renderer.setMap(mapManager.getCurrentMap());
+//            return;
+//        }
+//        gameState.update(action);
+//    }
     @Override
     public void show() {
 
@@ -104,7 +59,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        viewport.update(width, height, true);
+        renderer.windowResized(width, height);
     }
 
     @Override
@@ -124,8 +79,6 @@ public class PlayScreen implements Screen {
 
     @Override
     public void dispose() {
-        batch.dispose();
-        playerTexture.dispose();
-        mapManager.dispose();
+
     }
 }
