cdef3c82639275a39fa49e1b6ea92e18e7a436c5
Tom Owen <77887593+tomowen02@users.noreply.github.com>
2024-03-15 09:09:50 +0000

    Make UI display correctly on high res screen

--- desktop/src/com/heslingtonhustle/DesktopLauncher.java
+++ desktop/src/com/heslingtonhustle/DesktopLauncher.java
@@ -2,12 +2,14 @@ package com.heslingtonhustle;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.glutils.HdpiMode;
 import com.heslingtonhustle.HeslingtonHustleGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setHdpiMode(HdpiMode.Logical);
 		config.setForegroundFPS(60);
 		config.setTitle("HeslingtonHustle");
 		new Lwjgl3Application(new HeslingtonHustleGame(), config);
